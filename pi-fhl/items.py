4319aa7de9b8ecf3e4a61b31ca64b6c8d32ccc9f
Chris Liu <guohong.liu@gmail.com>
2020-02-18 17:04:10 +0800

    remove unused code

--- pi-fhl/items.py
+++ pi-fhl/items.py
@@ -4,10 +4,3 @@ from scrapy import Item, Field
 class RawItem(Item):
     url = Field()
     data = Field()
-
-
-class PoemItem(Item):
-    tid = Field()
-    title = Field()
-    sentences = Field()
-    author = Field()
